6184a57b35048bed392e44b3ee5885d90f02e8f0
alireza <comolestyrusistantaneos@gmail.com>
2023-01-24 11:05:15 +0330

    clean DotGroup

--- src/components/DotGroup.jsx
+++ src/components/DotGroup.jsx
@@ -5,46 +5,24 @@ const DotGroup = ({ selectedPage, setSelectedPage }) => {
   before:rounded-full before:border-2 before:border-yellow-500 before:left-[-50%] 
   before:top-[-50%]`;
 
+  const navCategories = [
+    "home",
+    "skills",
+    "projects",
+    "testimonials",
+    "contact",
+  ];
+
   return (
     <div className="flex flex-col gap-6 fixed top-[60%] right-7">
-      <AnchorLink
-        className={`${selectedPage === "home" ? selectedStyles : "bg-gray-500"}
+      {navCategories.map((cat) => {
+        <AnchorLink
+          className={`${selectedPage === cat ? selectedStyles : "bg-gray-500"}
             w-3 h-3 rounded-full`}
-        href="#home"
-        onClick={() => setSelectedPage("home")}
-      />
-      <AnchorLink
-        className={`${
-          selectedPage === "skills" ? selectedStyles : "bg-gray-500"
-        }
-            w-3 h-3 rounded-full`}
-        href="#skills"
-        onClick={() => setSelectedPage("skills")}
-      />
-      <AnchorLink
-        className={`${
-          selectedPage === "projects" ? selectedStyles : "bg-gray-500"
-        }
-            w-3 h-3 rounded-full`}
-        href="#projects"
-        onClick={() => setSelectedPage("projects")}
-      />
-      <AnchorLink
-        className={`${
-          selectedPage === "testimonials" ? selectedStyles : "bg-gray-500"
-        }
-            w-3 h-3 rounded-full`}
-        href="#testimonials"
-        onClick={() => setSelectedPage("testimonials")}
-      />
-      <AnchorLink
-        className={`${
-          selectedPage === "contact" ? selectedStyles : "bg-gray-500"
-        }
-            w-3 h-3 rounded-full`}
-        href="#contact"
-        onClick={() => setSelectedPage("contact")}
-      />
+          href={`#${cat}`}
+          onClick={() => setSelectedPage(cat)}
+        />;
+      })}
     </div>
   );
 };
