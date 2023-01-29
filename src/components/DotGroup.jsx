b7b61f8641028a4220960703b3f1e842608a1c4f
alireza <comolestyrusistantaneos@gmail.com>
2023-01-29 20:29:51 +0330

    fix on yellow dots

--- src/components/DotGroup.jsx
+++ src/components/DotGroup.jsx
@@ -1,8 +1,8 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const DotGroup = ({ selectedPage, setSelectedPage }) => {
-  const selectedStyles = `relative bg-yellow-500 before:absolute before:w-6 before:h-6 
-  before:rounded-full before:border-2 before:border-yellow-500 before:left-[-50%] 
+  const selectedStyles = `relative bg-yellow before:absolute before:w-6 before:h-6 
+  before:rounded-full before:border-2 before:border-yellow before:left-[-50%] 
   before:top-[-50%]`;
 
   return (
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,7 +5,7 @@ import profilePic from "../assets/profilePic.jpg";
 import SocialMediaIcons from "./SocialMediaIcons";
 
 const Landing = ({ setSelectedPage }) => {
-  const isAboveLarge = useMediaQuery("min-width: 1060px");
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
 
   return (
     <section
@@ -15,13 +15,13 @@ const Landing = ({ setSelectedPage }) => {
       <div className=" basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
         {isAboveLarge ? (
           <div
-            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 before:rounded-t-[400px] before:rounded-b-[400px]
             before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
               alt="profile"
               src={profilePic}
-              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-full"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[300px] md:max-w-[500px] rounded-full"
             />
           </div>
         ) : (
