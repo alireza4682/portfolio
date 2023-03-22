18636881a1ca2ff2b8dc8afd5b52e9a37e7433e9
Alireza <comolestyrusistantaneos@gmail.com>
2023-03-23 00:30:10 +0330

    test of dots

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,35 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
 import useMediaQuery from "./hooks/useMediaQuery";
+// import { useInView } from "framer-motion";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
+  const ref5 = useRef(null);
+
+  // const inView1 = useInView(ref1);
+  // const inView2 = useInView(ref1);
+  // const inView3 = useInView(ref1);
+  // const inView4 = useInView(ref1);
+  // const inView5 = useInView(ref1);
+
+  // const whatIsInView = () => {
+  //   if (inView1) setSelectedPage("home");
+  //   if (inView2) setSelectedPage("skills");
+  //   if (inView3) setSelectedPage("projects");
+  //   if (inView4) setSelectedPage("testimonials");
+  //   if (inView5) setSelectedPage("contacts");
+  //   console.log(selectedPage);
+  // };
+
+  // window.addEventListener("scroll", whatIsInView());
 
   useEffect(() => {
     const handleScroll = () => {
@@ -38,23 +62,23 @@ function App() {
             setSelectedPage={setSelectedPage}
           />
         )}
-        <Landing setSelectedPage={setSelectedPage} ref={landingRef} />
+        <Landing setSelectedPage={setSelectedPage} />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <MySkills ref={skillRef} />
+      <div className="w-5/6 mx-auto md:h-full" ref={ref2}>
+        <MySkills />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto ">
-        <Projects ref={projectRef} />
+      <div className="w-5/6 mx-auto " ref={ref3}>
+        <Projects />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <Testimonials ref={testimonialRef} />
+      <div className="w-5/6 mx-auto md:h-full" ref={ref4}>
+        <Testimonials />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <Contacts ref={contactRef} />
+      <div className="w-5/6 mx-auto md:h-full" ref={ref5}>
+        <Contacts />
       </div>
       <Footer />
     </div>
